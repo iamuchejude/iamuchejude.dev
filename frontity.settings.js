5037202452ddddc0796694f05e3a7f6c395bb842
Uche Jude <iamuchejude@gmail.com>
2020-01-28 04:57:46 +0100

    initialize frontity :fire:

---
+++ frontity.settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  name: "iamuchejude.dev",
+  state: {
+    frontity: {
+      url: "http://localhost:3000"
+    }
+  },
+  packages: [
+    '@frontity/tiny-router'
+  ]
+};
+
+export default settings;
