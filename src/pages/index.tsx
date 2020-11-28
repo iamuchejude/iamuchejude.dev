c3de84de92cb399b2e35bc2f6e52f92b6ff7aed2
Uche Jude <iamuchejude@gmail.com>
2020-11-28 13:14:01 +0100

    ch: switch technology

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
 import React, { FC } from 'react';
-import Layout from '../components/layout';
+import { Layout, Anchor } from '../components';
 
 const Index: FC = () => {
-  return <Layout>Hello, Stevelash</Layout>;
+  return (
+    <Layout>
+      I'm a{' '}
+      <Anchor href="https://google.com">Frontend Engineer</Anchor>,
+      I'm spent most of my day talking about{' '}
+      <Anchor href="https://google.com">UX Design</Anchor>. Now, I'm
+      either{' '}
+      <Anchor href="https://github.com/iamuchejude">coding</Anchor>,
+      liking or retweeting since I rarely . I'd also love to reach
+      more persons by{' '}
+      <Anchor href="https://twitter.com/iamuchejude">writing</Anchor>{' '}
+      on a personal blog or create video content on{' '}
+      <Anchor href="https://twitter.com/iamuchejude">YouTube</Anchor>.
+    </Layout>
+  );
 };
 
 export default Index;
