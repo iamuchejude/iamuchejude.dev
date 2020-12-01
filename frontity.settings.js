584cf8408465b722e0f1e53dcac3b9b94a0b2217
Uche Jude <iamuchejude@gmail.com>
2020-12-01 05:15:43 +0100

    ch: update styles

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,25 +1,25 @@
 const settings = {
-  name: "iamuchejude.dev",
+  name: 'iamuchejude.dev',
   state: {
     frontity: {
-      url: "https://iamuchejude.dev",
-      title: "Uche Jude",
-      description: "Web Frontend Engineer",
+      url: 'https://iamuchejude.dev',
+      title: 'Uche Jude',
+      description: 'Web Frontend Engineer',
     },
   },
   packages: [
-    "@iamuchejude/website",
+    '@iamuchejude/website',
     {
-      name: "@frontity/wp-source",
+      name: '@frontity/wp-source',
       state: {
         source: {
-          url: "http://iamuchejude.local",
-          homepage: "/home/",
+          url: 'http://iamuchejude.local',
+          homepage: '/home/',
         },
       },
     },
-    "@frontity/tiny-router",
-    "@frontity/html2react",
+    '@frontity/tiny-router',
+    '@frontity/html2react',
   ],
 };
 
---
+++ packages/website/src/styles.js
@@ -0,0 +1,49 @@
+import { css } from 'frontity';
+
+export const globalStyle = css`
+  :root {
+    --primary-color: #000;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-size: 20px;
+    font-weight: 300;
+    font-style: normal;
+    line-height: 1.6;
+    padding: 4rem 8rem;
+    font-family: 'Josefin Sans', sans-serif;
+    --webkit-font-smoothing: antialiased;
+  }
+`;
+
+export const externalLinksStyle = css`
+  text-decoration: none;
+  white-space: nowrap;
+  position: relative;
+  border-bottom: 1px solid #000;
+  transition: all 0.4s;
+  color: inherit;
+
+  &:hover {
+    color: #000;
+    border: none;
+    background-color: rgba(0, 0, 0, 0.15);
+  }
+`;
+
+/* crest <== quest ==> logo */
+export const questStyle = css`
+  margin: 0;
+  width: 70px;
+  height: 70px;
+  margin-bottom: 3rem;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
