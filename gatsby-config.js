94c5d27af9b9e4e27a64d6805413048ffbc54dd7
Uche Jude <iamuchejude@gmail.com>
2021-01-05 18:05:53 +0100

    ch: setup gatsby@2**

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteMetadata: {
+    title: `iamuchejude.dev`,
+    description: `Frontend Engineer & UX Enthusiast`,
+    author: `@iamuchejude`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `iamuchejude.dev`,
+        short_name: `iamuchejude`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`,
+      },
+    },
+  ],
+};
